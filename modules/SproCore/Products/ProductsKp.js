20cfd4254f469e41d1853df3c8973fb3f70248da
KproConsulting <37585246+KproConsulting@users.noreply.github.com>
2018-06-26 11:02:02 +0200

    Create ProductsKp.js

---
+++ modules/SproCore/Products/ProductsKp.js
@@ -0,0 +1,12 @@
+/* kpro@bid260620181100 */
+jQuery(document).ready(function() {
+
+	jQuery('input[type="checkbox"].small').each(function(){
+		var str_onclick = String(jQuery(this).prop('onclick'));
+		if(str_onclick.includes("tax2") || str_onclick.includes("tax3") || str_onclick.includes("tax4") || str_onclick.includes("tax5")){
+			jQuery(this).closest('tr').hide();
+		}
+	});
+
+});
+/* kpro@bid260620181100 end */
